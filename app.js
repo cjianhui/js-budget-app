e9b1407f60943658e1aa7199b14fff86b8ba2074
cjianhui <cjianhui1993@gmail.com>
2019-02-07 11:34:39 +0800

    Add new item to UI

--- app.js
+++ app.js
@@ -59,24 +59,49 @@ var budgetController = (function () {
 // UI Controller
 var UIController = (function () {
 
-    var DOMStrings = {
+    var DOMstrings = {
         inputType: 'add__type',
         inputDescription: 'add__description',
         inputValue: 'add__value',
-        inputBtn: 'add__btn'
+        inputBtn: 'add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
         getInput: function() {
             return {
-                type: document.getElementById(DOMStrings.inputType).value,
-                description: document.getElementById(DOMStrings.inputDescription).value,
-                value: document.getElementById(DOMStrings.inputValue).value
+                type: document.getElementById(DOMstrings.inputType).value,
+                description: document.getElementById(DOMstrings.inputDescription).value,
+                value: document.getElementById(DOMstrings.inputValue).value
             }
         },
 
         getDOMStrings: function() {
-            return DOMStrings;
+            return DOMstrings;
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="fas fa-trash"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="fas fa-trash"></i></button></div></div></div>';
+            }
+
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         }
     }
 
@@ -106,9 +131,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
-        budgetCtrl.addItem(input.type, input.description, input.value);
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
 
+        UICtrl.addListItem(newItem, input.type);
         // 4. Clear the fields
 
         // 5. Calculate and update budget
