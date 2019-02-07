9bfcd8df3b176190fbb7472635e64c644f8e6357
cjianhui <cjianhui1993@gmail.com>
2019-02-07 12:24:25 +0800

    Add input validation

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
@@ -136,6 +136,13 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function() {
+        // 1. Calculate and update budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget in UI
+    }
 
 
     var ctrlAddItem = function() {
@@ -144,18 +151,22 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 5. Calculate and update budget
+            //5. Calculate and update budget
+            updateBudget();
+        }
 
-        // 6. Calculate and update percentages
+
+
     };
 
     return {
